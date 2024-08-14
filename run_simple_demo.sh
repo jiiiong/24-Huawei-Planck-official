58b1488e6f71e030ee30158c56f2b743d2bd5616
jiiiong <jiiiong@qq.com>
2024-08-14 14:15:55 +0800

    1. 修改 ignore，忽略 log 和 __pycache__ 2. 删除其他版本的 solution 减少混淆 3. 新增 windows 版本的 replayer

--- run_simple_demo.sh
+++ run_simple_demo.sh
@@ -1,1 +1,3 @@
-./SemiFinalJudge "python3.7 ./solution/main.py" -m ./maps/map1.txt -s 0 -d input.txt
+# -m 指定地图路径
+# -s 指定随机种子，可以为空
+./SemiFinalJudge "python3.7 ./solution/main.py" -m ./maps/map3.txt -s 0
